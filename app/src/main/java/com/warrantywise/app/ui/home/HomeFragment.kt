6f635fbcf9eee6cbc0d1178002c6edcd60b6a06a
Devanshi Sharma <sharmadevanshi0116@gmail.com>
2026-08-29 20:49:47 +0530

    feat: build WarrantyWise home dashboard

--- app/src/main/java/com/warrantywise/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/warrantywise/app/ui/home/HomeFragment.kt
@@ -1,6 +1,22 @@
 package com.warrantywise.app.ui.home
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.warrantywise.app.R
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnAddWarranty =
+            view.findViewById<MaterialButton>(R.id.btnAddWarranty)
+
+        btnAddWarranty.setOnClickListener {
+            findNavController().navigate(R.id.nav_add_warranty)
+        }
+    }
+}
